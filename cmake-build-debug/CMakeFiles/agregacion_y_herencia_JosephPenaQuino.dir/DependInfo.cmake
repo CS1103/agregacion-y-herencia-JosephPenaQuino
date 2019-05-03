f94907162b72a50e655950853023996e22ed177c
JosephPenaQuino <joseph.pena@utec.edu.pe>
2019-05-03 12:21:13 -0500

    Test

---
+++ cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/Book.cpp" "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Book.cpp.o"
+  "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/Library.cpp" "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Library.cpp.o"
+  "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/Magazine.cpp" "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Magazine.cpp.o"
+  "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/Volume.cpp" "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Volume.cpp.o"
+  "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/main.cpp" "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/main.cpp.o"
+  "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/main_test.cpp" "/home/joseph/Documents/s2k191/POO2/Assignments/agregacion-y-herencia-JosephPenaQuino/cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/main_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/main.cpp.o"
+  "CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Volume.cpp.o"
+  "CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Book.cpp.o"
+  "CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Magazine.cpp.o"
+  "CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/Library.cpp.o"
+  "CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/main_test.cpp.o"
+  "agregacion_y_herencia_JosephPenaQuino.pdb"
+  "agregacion_y_herencia_JosephPenaQuino"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/agregacion_y_herencia_JosephPenaQuino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
